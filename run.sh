cbd5062d7c6b978ab72608a39ae89cbbc4912724
J <J>
2018-07-04 18:13:41 +0000

    more variations on run.sh

--- run.sh
+++ run.sh
@@ -8,4 +8,10 @@
 #sudo uwsgi --enable-threads --plugin=python3 -s /tmp/simple.sock --manage-script-name --mount /=wsgi:app --uid ubuntu --gid ubuntu
 
 # Change from "app" to "application"
-sudo uwsgi --enable-threads --plugin=python3 -s /tmp/simple.sock --manage-script-name --mount /=wsgi:application --uid ubuntu --gid ubuntu
+# sudo uwsgi --enable-threads --plugin=python3 -s /tmp/simple.sock --manage-script-name --mount /=wsgi:application --uid ubuntu --gid ubuntu
+
+# Change back to www-data
+#sudo uwsgi --enable-threads --plugin=python3 -s /tmp/simple.sock --manage-script-name --mount /=wsgi:application --uid www-data --gid www-data
+
+# Add -H flag
+sudo -H uwsgi --enable-threads --plugin=python3 -s /tmp/simple.sock --manage-script-name --mount /=wsgi:application --uid www-data --gid www-data
